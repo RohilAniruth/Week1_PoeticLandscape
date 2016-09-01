7004c5f780eb3dae6af303baa794b26cc79e69ad
Rohil Aniruth <anirr579@newschool.edu>
2016-09-01 18:30:43 -0400

    Week 1 of class. First bit of Unity work, PoeticLandscape and function tests.

---
+++ Assets/lightTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //when something enters the Trigger attached to this gameObject
+        void OnTriggerEnter() {
+        // get the light attached to game object
+        // turn light on
+        //functions need perenthesis
+        GetComponent<Light>().enabled = true;
+
+
+        //GetComponent<Light>().color = Color.cyan; ALT LIGHT FUNCTION TO KEEP LIGHT ON
+
+
+    }
+
+    // get light to turn off when you leave
+    void OnTriggerExit()
+    {
+        GetComponent<Light>().enabled = false; 
+      //GetComponent<Light>().color = Color.magenta; ALT LIGHT FUNCTION TO KEEP LIGHT ON
+    }
+}
